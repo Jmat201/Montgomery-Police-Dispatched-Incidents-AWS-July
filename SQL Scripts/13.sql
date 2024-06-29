98d6a7738f8e987f5b54f6850cb6ea79c9fa0229
Jmat201 <37741391+Jmat201@users.noreply.github.com>
2024-06-29 15:08:27 -0400

    SQL for Dashboard 13

---
+++ SQL Scripts/13.sql
@@ -0,0 +1,8 @@
+SELECT 
+    initial_type, count(distinct incident_id)count_incidents
+FROM 
+    "de_project_database"."project_montgomery_police_dispatch_incidents"
+WHERE 
+    TRIM(SUBSTR(address, POSITION('BLK' IN address) + 3)) = 'ROCKVILLE PIKE'
+group by 1 
+order by 2 desc
