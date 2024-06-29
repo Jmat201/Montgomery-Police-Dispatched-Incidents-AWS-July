eb8879a8c53da316fbec3d70f0f67c106aeed730
Jmat201 <37741391+Jmat201@users.noreply.github.com>
2024-06-29 15:15:02 -0400

    SQL for Dashboard 5

---
+++ SQL Scripts/19.sql
@@ -0,0 +1,25 @@
+
+WITH RankedResults AS (
+    SELECT 
+        CONCAT(initial_type, ' : ', disposition_desc) AS type_disposition,
+        COUNT(DISTINCT incident_id) AS incident_count,
+        ROW_NUMBER() OVER (PARTITION BY initial_type ORDER BY COUNT(DISTINCT incident_id) DESC) AS rank
+    FROM 
+        "de_project_database"."montgomery_police_inc_data_parquet_tbl"
+    WHERE 
+        city = 'ROCKVILLE' 
+        AND initial_type IN ('TRAFFIC/TRANSPORTATION INCIDENT'
+                ,'DISTURBANCE/NUISANCE'
+                ,'TRESPASSING/UNWANTED'
+                ,'CHECK WELFARE')    GROUP BY 
+        initial_type, disposition_desc
+)
+SELECT 
+    type_disposition,
+    incident_count
+FROM 
+    RankedResults
+WHERE 
+    rank <= 3
+ORDER BY 
+    type_disposition, incident_count DESC;
