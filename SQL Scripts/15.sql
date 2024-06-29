f448c857ada9719379a26b699a641d809e477466
Jmat201 <37741391+Jmat201@users.noreply.github.com>
2024-06-29 15:11:24 -0400

    SQL for Dashboard 15

---
+++ SQL Scripts/15.sql
@@ -0,0 +1,9 @@
+SELECT 
+    initial_type, count(distinct incident_id)count_incidents
+
+FROM 
+    "de_project_database"."project_montgomery_police_dispatch_incidents"
+WHERE 
+    TRIM(SUBSTR(address, POSITION('BLK' IN address) + 3)) = 'VEIRS MILL RD'
+group by 1 
+order by 2 desc
