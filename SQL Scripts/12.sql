f3f33a3d88bb62b566ef2dae5024272b695ff878
Jmat201 <37741391+Jmat201@users.noreply.github.com>
2024-06-29 15:07:29 -0400

    SQL for Dashboard 12

---
+++ SQL Scripts/12.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT
+    incident_id, 
+    geolocation_coordinates[2] AS latitude,
+    geolocation_coordinates[1] AS longitude
+FROM 
+    "de_project_database"."project_montgomery_police_dispatch_incidents"
+WHERE 
+    TRIM(SUBSTR(address, POSITION('BLK' IN address) + 3)) = ‘ROCKVILLE PIKE’
