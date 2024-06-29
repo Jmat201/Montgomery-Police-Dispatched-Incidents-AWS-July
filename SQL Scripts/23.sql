ae2f3c2fa88b1c6a5c786de43684b6f7e84269dc
Jmat201 <37741391+Jmat201@users.noreply.github.com>
2024-06-29 15:16:58 -0400

    SQL for Dashboard 23

---
+++ SQL Scripts/23.sql
@@ -0,0 +1,9 @@
+SELECT 
+sector,
+count(distinct incident_id) count_incidents
+FROM  
+    "de_project_database"."montgomery_police_inc_data_parquet_tbl"
+WHERE dispatch_arrive = '' and arrive_cleared = ''
+group by 1
+order by 2 desc
+limit 8
